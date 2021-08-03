431f937ae366a0ae6763ba084456aacf50a670ad
Pavel Manylov <pavel@manylov.me>
2021-08-03 22:55:17 +0500

    MTP-8 API обновления и удаления компонента: дополнение документации по сервисному классу

--- app/interactions/component/update_component.rb
+++ app/interactions/component/update_component.rb
@@ -1,8 +1,28 @@
+# Сервисный класс обновления компонента
+# @example
+#
+#   outcome = UpdateComponent.run component_id: 199,
+#                                 public: "true",
+#                                 type: "relation",
+#                                 value: [{"id" => 123}],
+#                                 order: 0,
+#                                 post_id: 12,
+#                                 custom_fields: {"preview" => "lorem imsum"}
+#   outcome.errors.to_a # => [] or ["Some error #1"]
+#   outcome.result # => Component (может быть несохранённым при ошибках)
+#
 class UpdateComponent < ActiveInteraction::Base
   include ModifyComponent
 
+  # @!attribute component_id
+  #   Идентификатор компонента
+  #
+  #   @validation Обязателен для заполнения
+  #   @validation Соответствует существующему компоненту
+  #   @return [Integer]
   integer :component_id
 
+  # Обновляет компонент
   def execute
     component = compose(GetComponent, component_id: component_id)
 
